84fdc39d5b7c3e63002390d403790074d37fdf71
Rolisthenics Orlando <rolisthenicsorlando@outlook.com>
2025-03-23 00:45:41 +0100

    project setup done

---
+++ backend/src/main/java/gorilla_kitchen/stabiles_essen/StabilesEssenApplication.java
@@ -0,0 +1,12 @@
+package gorilla_kitchen.stabiles_essen;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class StabilesEssenApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StabilesEssenApplication.class, args);
+	}
+}
+
---
+++ backend/src/test/java/gorilla_kitchen/stabiles_essen/StabilesEssenApplicationTests.java
@@ -0,0 +1,13 @@
+package gorilla_kitchen.stabiles_essen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StabilesEssenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
